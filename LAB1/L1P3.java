eaf3399e709922954d0f6844c4ee68b3b431c6b3
Sumedhavadali <sumedhavadali@gmail.com>
2026-01-20 16:34:46 +0530

    Add files via upload

---
+++ LAB1/L1P3.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+	class L1P3{
+		public static void main(String[] args){
+			int x=10;
+			//System.out.println("integer value "+ x +"");
+			double D= 15.12;
+			//float y=15.12;
+			float y=(float)D;
+			//System.out.println("float value " + y+" ");
+			double d=100.40;
+			//System.out.println("double value " +d+" ");
+			char ch='a';
+			Boolean b= true;
+			byte by=6;
+			short s=12;
+			long l=58;
+			System.out.println("integer value "+ x + "\nfloat value "+y+ "\ndouble value"+d+ " \ncharacter value "+ch+ " \nboolean value "+b+ "\nbyte value "+ by + "\nshort "+ s +"\nlong value "+l+" ");
+			
+}
+
+}
