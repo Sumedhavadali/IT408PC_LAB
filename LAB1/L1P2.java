e38d4904c82127449a27de1073a7927395b9a5f2
Sumedhavadali <sumedhavadali@gmail.com>
2026-01-20 16:34:12 +0530

    Add files via upload

---
+++ LAB1/L1P2.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+	class L1P2{
+		public static void main(String[] args){
+			int x=25;
+			double d=x;
+			System.out.println("Integer value "+ x + " Double Value "+ x +" ");
+			double D=52.258;
+			int y =(int) D;
+			System.out.println("Double value "+ y + " Integer Value "+ y +" ");
+
+	}
+
+}
